7a1bb9f016567134619d1a2e5c27eb04a0a58259
JOJO <jolinwangt@gmail.com>
2025-07-23 13:39:42 -0700

    Initial Commit

---
+++ InterativeUITests/InterativeUITests.swift
@@ -0,0 +1,17 @@
+//
+//  InterativeUITests.swift
+//  InterativeUITests
+//
+//  Created by Jolin Wang on 7/23/25.
+//
+
+import Testing
+@testable import InterativeUI
+
+struct InterativeUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
